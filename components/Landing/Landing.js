59283992857c74b0dee709537297c897335793ad
Amil Silahic <jinobie02@gmail.com>
2023-04-01 17:34:32 -0600

    feat: :zap: Created Landing About Us Section
    Created landing about us section and all related styling and etc

--- components/Landing/Landing.js
+++ components/Landing/Landing.js
@@ -1,9 +1,11 @@
+import LandingAbout from "./LandingAbout";
 import LandingHero from "./LandingHero";
 
 const Landing = ({}) => {
   return (
     <>
       <LandingHero />
+      <LandingAbout />
     </>
   );
 };
---
+++ components/Landing/LandingAbout.js
@@ -0,0 +1,44 @@
+import Image from "next/image";
+
+const LandingAbout = ({}) => {
+  return (
+    <>
+      <section>
+        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8  ">
+          <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16 ">
+            <div className="relative h-64 overflow-hidden rounded-lg sm:h-80  lg:h-full">
+              <div className="h-full w-full relative m-auto flex justify-center items-center">
+                <Image
+                  alt="Photo of House"
+                  src="/images/workplace.jpeg"
+                  width={500}
+                  height={1000}
+                  className="z-50 h-full object-cover w-full"
+                />
+              </div>
+            </div>
+
+            <div className="lg:py-24">
+              <h1 className="text-6xl font-bold sm:text-4xl w-1/2">About Us</h1>
+
+              <p className="mt-4 text-[#5F5B53] w-auto text-md ">
+                We are a company that connects the world of real estate. We
+                provide a complete service for the sale, purchase or rental of
+                real estate. Our advantage is more than 15 years of experience
+                and soil in attractive locations across the world!
+                <br />
+                <br />
+                We have a connection to all banks on the market, so we can solve
+                everything under one roof. By constantly innovating our business
+                activities, we move forward and we are able to offer truly
+                above-standard services that set us apart from the competition.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default LandingAbout;
