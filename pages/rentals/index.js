e8372763c8c402121d52b43e5392b661c0e0cf00
Amil Silahic <jinobie02@gmail.com>
2023-04-03 12:19:39 -0600

    feat: :art: Added Addresses to Supabase table
    Added addresses to sql table. Next step is to add people table and connect pKey from addresses to there

--- pages/rentals/index.js
+++ pages/rentals/index.js
@@ -1,11 +1,26 @@
-const AllRentalsPage = ({}) => {
+import { supabase } from "@/lib/supabase";
+
+const AllRentalsPage = ({ rentals }) => {
   return (
     <>
       <div>
         <h1>All Rentals Page</h1>
+        {rentals.map((item) => {
+          return <h1 key={item.uid}>{item.time_zone}</h1>;
+        })}
       </div>
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const { data, error } = await supabase.from("addresses").select();
+
+  return {
+    props: {
+      rentals: data,
+    },
+  };
 };
 
 export default AllRentalsPage;
