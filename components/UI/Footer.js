ab6de6d78c2ac2769f8b54d3e17ff3b30185ed1e
Amil Silahic <jinobie02@gmail.com>
2023-04-08 15:06:54 -0600

    putting shadow on footer to make it stand out more

--- components/UI/Footer.js
+++ components/UI/Footer.js
@@ -9,7 +9,7 @@ import Link from "next/link";
 
 const Footer = ({}) => {
   return (
-    <footer className="relative bg-blueGray-200 pt-8 pb-6">
+    <footer className="relative bg-blueGray-200 pt-8 pb-6 shadow-inner">
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap text-left lg:text-left">
           <div className="w-full lg:w-6/12 px-4">
