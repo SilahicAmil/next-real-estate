f0393bf1d0fe3fd3e8b363d68ec675ccdf8a5212
Amil Silahic <jinobie02@gmail.com>
2023-04-10 15:06:19 -0600

    misc update

--- components/RentalsDetails/PhotoCarousel.js
+++ components/RentalsDetails/PhotoCarousel.js
@@ -1,7 +1,6 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import { Carousel } from "react-responsive-carousel";
-import Image from "next/image";
 import PhotoItem from "./PhotoItem";
 
 const PhotoCarousel = ({ imageData }) => {
@@ -9,32 +8,11 @@ const PhotoCarousel = ({ imageData }) => {
     <>
       <div className="lg:w-1/2">
         <Carousel showArrows={true} showThumbs={false}>
-          {/* Map Over PhotoItem */}
+          {/* Map Over PhotoItem Eventually */}
           <PhotoItem />
-          <div>
-            <Image
-              alt="123"
-              src="/images/test-photo.jpeg"
-              width={400}
-              height={400}
-            />
-          </div>
-          <div>
-            <Image
-              alt="123"
-              src="/images/test-photo.jpeg"
-              width={400}
-              height={400}
-            />
-          </div>
-          <div>
-            <Image
-              alt="123"
-              src="/images/test-photo.jpeg"
-              width={400}
-              height={400}
-            />
-          </div>
+          <PhotoItem />
+          <PhotoItem />
+          <PhotoItem />
         </Carousel>
       </div>
     </>
