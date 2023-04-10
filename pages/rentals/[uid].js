b298177b22da8216755807bbb5d40cd0dfabaf90
Amil Silahic <jinobie02@gmail.com>
2023-04-10 13:25:42 -0600

    updating styling

--- pages/rentals/[uid].js
+++ pages/rentals/[uid].js
@@ -15,7 +15,7 @@ const RentalsDetailsPage = ({ rental }) => {
       </div>
       <div className="lg:flex justify-center gap-24 mb-12 mt-12 h-full shadow-inner pt-12">
         <PhotoCarousel />
-        <div className="lg:w-1/4 flex-col flex items-center bg-red-500 rounded-md">
+        <div className="lg:w-1/4 flex-col flex items-center bg-red-500 rounded-md sm:mt-12 lg:mt-0">
           <h1>{rental.landlord_name}</h1>
           <h1>{rental.landlord_email}</h1>
         </div>
