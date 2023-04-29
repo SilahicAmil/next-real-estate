a03ade210af5ddd5992f70e78a75c54cf85e7133
Amil Silahic <jinobie02@gmail.com>
2023-04-29 14:09:54 -0600

    changing how stripe API req.method is handled

--- pages/api/checkout-session.js
+++ pages/api/checkout-session.js
@@ -1,39 +1,38 @@
 const stripe = require("stripe")(process.env.NEXT_PUBLIC_SECRET_KEY);
 
 const helper = async (req, res) => {
-  if (req.method === "POST") {
-    try {
-      const { amount, price, name, description } = req.body;
-
-      const transformedItem = {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: name,
-            description: description,
-          },
-          unit_amount: price * 100,
-        },
-        quantity: amount,
-      };
-
-      console.log(transformedItem);
-
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        line_items: [transformedItem],
-        mode: "payment",
-        success_url: `${req.headers.origin}/?status=success`,
-        cancel_url: `${req.headers.origin}/?status=cancel`,
-      });
-      res.status(201).json({ url: session.url });
-    } catch (err) {
-      res.status(err.statusCode || 500).json(err.message);
-    }
-  } else {
+  if (req.method !== "POST") {
     res.setHeader("Allow", "POST");
 
     res.status(405).end("Method Not Allowed");
+  }
+  try {
+    const { amount, price, name, description } = req.body;
+
+    const transformedItem = {
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: name,
+          description: description,
+        },
+        unit_amount: price * 100,
+      },
+      quantity: amount,
+    };
+
+    console.log(transformedItem);
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [transformedItem],
+      mode: "payment",
+      success_url: `${req.headers.origin}/?status=success`,
+      cancel_url: `${req.headers.origin}/?status=cancel`,
+    });
+    res.status(201).json({ url: session.url });
+  } catch (err) {
+    res.status(err.statusCode || 500).json(err.message);
   }
 };
 
