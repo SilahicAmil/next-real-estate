668a0e47ed5e42a0d6395cc28e6eefe7ed1f7626
Amil Silahic <jinobie02@gmail.com>
2023-04-29 14:06:17 -0600

    updating how mapbox WebGL is handled

--- components/AllRentals/RentalsMap.js
+++ components/AllRentals/RentalsMap.js
@@ -15,42 +15,41 @@ const RentalsMap = () => {
     if (mapboxgl.supported() === false) {
       setWebglError(true);
       return;
-    } else {
-      const map = new mapboxgl.Map({
-        container: mapContainer.current,
-        style: process.env.NEXT_PUBLIC_MAP_STYLE,
-        center: [-90.791303, 40.267571],
-        zoom: 4,
-        cooperativeGestures: true,
-      });
-
-      map.on("load", () => {
-        setMapInstance(map);
-
-        // map.resize();
-      });
-
-      map.on("click", (event) => {
-        const features = map.queryRenderedFeatures(event.point, {
-          layers: ["updated-real-estate"],
-        });
-
-        if (!features.length) {
-          return;
-        }
-
-        const feature = features[0];
-
-        new mapboxgl.Popup({ offset: [0, -15] })
-          .setLngLat(feature.geometry.coordinates)
-          .setHTML(
-            `<h3>${feature.properties.address}</h3><a href="rentals/${feature.properties.uid}" rel="noreferrer" target="_blank">View Property</a>`
-          )
-          .addTo(map);
-      });
-
-      map.addControl(new mapboxgl.NavigationControl());
     }
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: process.env.NEXT_PUBLIC_MAP_STYLE,
+      center: [-90.791303, 40.267571],
+      zoom: 4,
+      cooperativeGestures: true,
+    });
+
+    map.on("load", () => {
+      setMapInstance(map);
+
+      // map.resize();
+    });
+
+    map.on("click", (event) => {
+      const features = map.queryRenderedFeatures(event.point, {
+        layers: ["updated-real-estate"],
+      });
+
+      if (!features.length) {
+        return;
+      }
+
+      const feature = features[0];
+
+      new mapboxgl.Popup({ offset: [0, -15] })
+        .setLngLat(feature.geometry.coordinates)
+        .setHTML(
+          `<h3>${feature.properties.address}</h3><a href="rentals/${feature.properties.uid}" rel="noreferrer" target="_blank">View Property</a>`
+        )
+        .addTo(map);
+    });
+
+    map.addControl(new mapboxgl.NavigationControl());
   }, []);
 
   return (
