6b386a3ff753200bd613b4951111e686df3c977e
Amil Silahic <jinobie02@gmail.com>
2023-04-29 14:05:34 -0600

    feat: :art: Adding WebGL error handling
    adding error handling if WebGL is not not enabled

--- components/AllRentals/RentalsMap.js
+++ components/AllRentals/RentalsMap.js
@@ -8,51 +8,65 @@ mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN;
 
 const RentalsMap = () => {
   const [mapInstance, setMapInstance] = useState(null);
+  const [webglError, setWebglError] = useState(null);
   const mapContainer = useRef();
 
   useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: process.env.NEXT_PUBLIC_MAP_STYLE,
-      center: [-90.791303, 40.267571],
-      zoom: 4,
-      cooperativeGestures: true,
-    });
-
-    map.on("load", () => {
-      setMapInstance(map);
-
-      // map.resize();
-    });
-
-    map.on("click", (event) => {
-      const features = map.queryRenderedFeatures(event.point, {
-        layers: ["updated-real-estate"],
+    if (mapboxgl.supported() === false) {
+      setWebglError(true);
+      return;
+    } else {
+      const map = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: process.env.NEXT_PUBLIC_MAP_STYLE,
+        center: [-90.791303, 40.267571],
+        zoom: 4,
+        cooperativeGestures: true,
       });
 
-      if (!features.length) {
-        return;
-      }
+      map.on("load", () => {
+        setMapInstance(map);
 
-      const feature = features[0];
+        // map.resize();
+      });
 
-      new mapboxgl.Popup({ offset: [0, -15] })
-        .setLngLat(feature.geometry.coordinates)
-        .setHTML(
-          `<h3>${feature.properties.address}</h3><a href="rentals/${feature.properties.uid}" rel="noreferrer" target="_blank">View Property</a>`
-        )
-        .addTo(map);
-    });
+      map.on("click", (event) => {
+        const features = map.queryRenderedFeatures(event.point, {
+          layers: ["updated-real-estate"],
+        });
 
-    map.addControl(new mapboxgl.NavigationControl());
+        if (!features.length) {
+          return;
+        }
+
+        const feature = features[0];
+
+        new mapboxgl.Popup({ offset: [0, -15] })
+          .setLngLat(feature.geometry.coordinates)
+          .setHTML(
+            `<h3>${feature.properties.address}</h3><a href="rentals/${feature.properties.uid}" rel="noreferrer" target="_blank">View Property</a>`
+          )
+          .addTo(map);
+      });
+
+      map.addControl(new mapboxgl.NavigationControl());
+    }
   }, []);
 
   return (
     <>
-      <div
-        className="h-[100vh] lg:h-[100vh] lg:w-full rounded-lg mt-12 m-4 "
-        ref={mapContainer}
-      />
+      {webglError ? (
+        <div className="lg:w-full rounded-lg mt-12 m-4">
+          <h1 className="text-2xl tracking-wider">
+            Please Enable WebGL to View Map.
+          </h1>
+        </div>
+      ) : (
+        <div
+          className="h-[100vh] lg:h-[100vh] lg:w-full rounded-lg mt-12 m-4 "
+          ref={mapContainer}
+        />
+      )}
     </>
   );
 };
