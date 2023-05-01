1ecf2a1a4f821d34baf4e88180c2203b12ad1e45
Amil Silahic <jinobie02@gmail.com>
2023-04-30 22:00:31 -0600

    re-structing the code

--- pages/api/checkout-session.js
+++ pages/api/checkout-session.js
@@ -3,24 +3,24 @@ const stripe = require("stripe")(process.env.NEXT_PUBLIC_SECRET_KEY);
 const helper = async (req, res) => {
   if (req.method !== "POST") {
     res.setHeader("Allow", "POST");
-
     res.status(405).end("Method Not Allowed");
   }
-  try {
-    const { amount, price, name, description } = req.body;
 
-    const transformedItem = {
-      price_data: {
-        currency: "usd",
-        product_data: {
-          name: name,
-          description: description,
-        },
-        unit_amount: price * 100,
+  const { amount, price, name, description } = req.body;
+
+  const transformedItem = {
+    price_data: {
+      currency: "usd",
+      product_data: {
+        name: name,
+        description: description,
       },
-      quantity: amount,
-    };
+      unit_amount: price * 100,
+    },
+    quantity: amount,
+  };
 
+  try {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       line_items: [transformedItem],
