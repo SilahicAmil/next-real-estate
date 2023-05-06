8221edf7e7d4bf07d96e987f2baf40974ad01628
Amil Silahic <jinobie02@gmail.com>
2023-05-06 13:44:33 -0600

    feat: :art: Adding QR COde
    Adding QR Code and removing QR Code Component and just using directly in page route

--- pages/confirmation/index.js
+++ pages/confirmation/index.js
@@ -1,6 +1,8 @@
 import Header from "@/components/UI/Header";
+import { useQRCode } from "next-qrcode";
 
 const ConfirmationPage = ({}) => {
+  const { Canvas } = useQRCode();
   return (
     <>
       <div>
@@ -8,6 +10,21 @@ const ConfirmationPage = ({}) => {
           title="Thank you for your purchase"
           subtext="We hope you enjoy your rental!"
         />
+        <div className="flex items-center w-full justify-center">
+          <Canvas
+            text="Test QR Code"
+            options={{
+              level: "L",
+              margin: 2,
+              scale: 5,
+              width: 250,
+              color: {
+                dark: "#010599FF",
+                light: "#FFBF60FF",
+              },
+            }}
+          />
+        </div>
       </div>
     </>
   );
