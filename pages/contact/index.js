f9870af84e5ad96a178e50776376c7ed1ab976ee
Amil Silahic <jinobie02@gmail.com>
2023-05-15 15:19:22 -0600

    adding form to /contact - next step is to add styling and functionality

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "@/components/UI/Header";
 
 const ContactUsPage = ({}) => {
   return (
@@ -12,9 +13,35 @@ const ContactUsPage = ({}) => {
         />
       </Head>
 
+      <Header
+        title="Contact Us"
+        subtext="Let us know any issues you encounter!"
+      />
+
       <div>
-        <h1>Contact Us Page</h1>
-        {/* typical contact form will go here */}
+        <form>
+          <div>
+            <label htmlFor="full-name">Full Name</label>
+            <input type="text" placeholder="Jim Bob" id="full-name" />
+          </div>
+
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="text" placeholder="jimbob@realestate.com" id="email" />
+          </div>
+
+          <div>
+            <label htmlFor="message">Message</label>
+            <textarea
+              id="message"
+              cols="30"
+              rows="10"
+              placeholder="Type your message here..."
+            />
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </>
   );
